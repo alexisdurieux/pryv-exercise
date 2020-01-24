d0c17ed068c43c3c99f8cea5cc5d8b5b62b85247
Alexis Durieux <adurieux@mediarithmics.com>
2020-01-25 00:55:30 +0100

    intersection exercise

--- src/controllers/DataController.ts
+++ src/controllers/DataController.ts
@@ -6,8 +6,10 @@ import {
     StreamsResponse,
     Event,
     EventResponse,
+    Stream,
 } from '../models';
 import { Service } from '../services/IService';
+// import { _ } from 'lodash';
 
 @Controller('data')
 export class DataController {
@@ -62,7 +64,67 @@ export class DataController {
                 message,
             });
         }
+    }
 
+    @Post('intersection')
+    private async streamIntersection(req: Request, res: Response) {
+        try {
+            const credentials: DuplicationCredentials = req.body;
+
+            if (!credentials.backup || !credentials.source) {
+                throw new Error('Invalid credentials');
+            }
+
+            const sourceService = new Service(credentials.source);
+            const backupService = new Service(credentials.backup);
+
+            const streamsPromises = await Promise.all(
+                [
+                    sourceService.get<StreamsResponse>('streams', {}),
+                    backupService.get<StreamsResponse>('streams', {}),
+                ],
+            );
+
+            const sourceStreams = streamsPromises[0] as StreamsResponse;
+            const backupStreams = streamsPromises[1] as StreamsResponse;
+
+            const newEvent: Event = {
+                streamId: backupStreams.streams[0].id,
+                type: 'exercice/intersection',
+                content: this.intersection(sourceStreams.streams, backupStreams.streams),
+            };
+
+            Logger.Info(JSON.stringify(newEvent));
+
+            const eventResponse = await backupService.post<EventResponse>('events', newEvent);
+            return res.status(200).json(eventResponse);
+        } catch (error) {
+            const message = error.toString();
+            Logger.Err(message);
+            return res.status(400).json({
+                message,
+            });
+        }
+
+    }
+
+    private intersection(streams1: Stream[], streams2: Stream[]): Stream[] {
+        const intersections: Stream[] = [];
+        streams1.forEach((stream1: Stream) => {
+            streams2.forEach((stream2: Stream) => {
+                if (stream1.id === stream2.id) {
+                    const cpy = JSON.parse(JSON.stringify(stream1));
+                    cpy.children = [];
+                    intersections.push(stream1);
+                }
+            });
+        });
+        intersections.forEach((stream1: Stream) => {
+            const stream2 = streams2.find((s) => s.id === stream1.id);
+            stream1.children = this.intersection(stream1.children, (stream2 as Stream).children);
+        });
+
+        return intersections;
     }
 
 }
--- src/start.ts
+++ src/start.ts
@@ -4,4 +4,3 @@ const server = new PryvServer();
 const app = server.start(3000);
 
 export default app;
-
